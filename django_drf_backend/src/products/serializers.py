92788e465fedc2443833bdb0858bce932dc95d7c
doer <499361328@qq.com>
2019-09-03 13:01:04 +0800

    django_drf_backend: 9 - Product & Variation Serializers

--- django_drf_backend/src/products/serializers.py
+++ django_drf_backend/src/products/serializers.py
@@ -1,10 +1,38 @@
 from rest_framework import serializers
 
-from .models import Category
+from .models import Category, Product, Variation
+
+
+class VariationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Variation
+        fields = [
+            "id",
+            "title",
+            "price",
+        ]
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    variation_set = VariationSerializer(many=True, read_only=True)
+    image = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Product
+        fields = [
+            "id",
+            "title",
+            "image",
+            "variation_set",
+        ]
+
+    def get_image(self, obj):
+        return obj.productimage_set.first().image.url
 
 
 class CategorySerializer(serializers.ModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='category_detail_api')
+    product_set = ProductSerializer(many=True)
 
     class Meta:
         model = Category
@@ -13,4 +41,6 @@ class CategorySerializer(serializers.ModelSerializer):
             "id",
             "title",
             "description",
+            "product_set",  # obj.product_set.all()
+            "default_category",
         ]
